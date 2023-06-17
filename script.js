a9ef33044adcc722bfa6b9cf0df7a4ae8dacac12
raihanariaputrab <arpb.business@gmail.com>
2023-06-17 18:09:05 +0700

    finish

---
+++ script.js
@@ -0,0 +1,23 @@
+function kirimData() {
+    var name = document.getElementById("nama").value;
+    var nim = document.getElementById("nim").value;
+    var peminatan = document.querySelector("input[name=peminatan]:checked").value;
+    var alamat = document.getElementById("alamat").value;
+    var angkatan = document.getElementById("angkatan").value;
+    var tanggal = document.getElementById("tanggal").value;
+  
+    alert(
+      "Nama : " +
+        name +
+        "\nNIM : " +
+        nim +
+        "\nPeminatan : " +
+        peminatan +
+        "\nAlamat: " +
+        alamat +
+        "\nAngkatan: " +
+        angkatan +
+        "\nTanggal: " +
+        tanggal
+    );
+  }
